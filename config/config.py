178fd6346cbeb3db39db7aee5f8bbcdf435024ee
Raymond Lam <raymondlam197@gmail.com>
2018-09-19 20:48:33 -0400

    Read env variables instead of hard code

--- config/config.py
+++ config/config.py
@@ -2,8 +2,8 @@ import os
 
 SECRET_KEY = 'A93wiqp8Jqnei983Ji82P0192NuqheNJi938J'
 
-MYSQL_USER = 'root' #os.environ['MYSQL_USER']
-MYSQL_PASSWORD = 'password' #os.environ['MYSQL_PASSWORD']
+MYSQL_USER = os.environ['MYSQL_USER']
+MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
 SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@localhost/testing_db'.format(MYSQL_USER, MYSQL_PASSWORD)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
